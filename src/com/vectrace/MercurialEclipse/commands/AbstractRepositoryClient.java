d8a4582f2763268df81c646a57db4b4ff8d1eb5e
Bastian Doetsch <Bastian Doetsch@localhost>
2008-05-30 20:47:06 +0200

    Implemented URI handling in HgRepositoryLocation
    ... as I should have done before. Now username and password are encapsulated in the HgRepositoryLocation object. This even cleans up some Hg*Client code :-). Side-effect: No MalformedUrlException, but a URISyntaxException instead. That's why so many classes had to
    changed.

--- src/com/vectrace/MercurialEclipse/commands/AbstractRepositoryClient.java
+++ src/com/vectrace/MercurialEclipse/commands/AbstractRepositoryClient.java
@@ -10,11 +10,6 @@
  *******************************************************************************/
 package com.vectrace.MercurialEclipse.commands;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-
-import com.vectrace.MercurialEclipse.storage.HgRepositoryLocation;
 
 /**
  * @author bastian
@@ -22,30 +17,6 @@ import com.vectrace.MercurialEclipse.storage.HgRepositoryLocation;
  */
 public class AbstractRepositoryClient extends AbstractClient {
 
-    /**
-     * @param location
-     * @param user
-     * @param pass
-     * @return
-     * @throws URISyntaxException
-     */
-    protected static URI getRepositoryURI(HgRepositoryLocation location, String user,
-            String pass) throws URISyntaxException {
-                URL url = location.getUrlObject();
-                String userInfo = user;
-                if (user != null && user.length() == 0) {
-                    // URI parts are undefinied, if they are null.
-                    userInfo = null;
-                } else if (user != null) {
-                    // pass gotta be separated by a colon
-                    if (pass != null && pass.length() != 0) {
-                        userInfo = userInfo.concat(":").concat(pass);
-                    }
-                }
-            
-                URI uri = new URI(url.getProtocol(), userInfo, url.getHost(), url
-                        .getPort(), url.getPath(), null, null);
-                return uri;
-            }
+
 
 }
