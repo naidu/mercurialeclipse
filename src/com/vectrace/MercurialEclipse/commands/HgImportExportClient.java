e6cc60e0d2619bdb211f73c40d3210bef9584c64
steeven <steeven@localhost>
2008-10-26 20:05:58 +0800

    export patch feature, to be refined

--- src/com/vectrace/MercurialEclipse/commands/HgImportExportClient.java
+++ src/com/vectrace/MercurialEclipse/commands/HgImportExportClient.java
@@ -10,7 +10,12 @@
  *******************************************************************************/
 package com.vectrace.MercurialEclipse.commands;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 
 import com.vectrace.MercurialEclipse.exception.HgException;
 
@@ -23,4 +28,29 @@ public class HgImportExportClient {
         return command.executeToString();
     }
 
+    public static String exportPatch(IProject project,
+            List<IResource> resources, String patchLocation) throws HgException {
+        HgCommand command = new HgCommand("diff", project, true);
+        // TODO command.addFiles(resources);
+        byte[] result = command.executeToBytes();
+        if (patchLocation != null)
+            saveFile(patchLocation, result);
+        return new String(result);
+    }
+
+    private static void saveFile(String patchLocation, byte[] result) throws HgException {
+        FileOutputStream stream = null;
+        try {
+            stream = new FileOutputStream(patchLocation);
+            stream.write(result);
+        } catch (IOException e) {
+            throw new HgException(e.getLocalizedMessage(), e);
+        } finally {
+            if (stream != null)
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                }
+        }
+    }
 }
